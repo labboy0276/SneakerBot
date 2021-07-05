0ece372d149f7d86f23a6061a067f5152f5d4b5c
John Ouellet <john@thinktandem.io>
2021-07-05 17:05:36 -0400

    More tweaks:

--- sites/footsites.js
+++ sites/footsites.js
@@ -353,7 +353,10 @@ exports.guestCheckout = async ({
 
       taskLogger.info('Selecting size');
       const sizesSelector = 'div.c-form-field.c-form-field--radio.ProductSize';
-      await page.waitForSelector(sizesSelector, { timeout: 30000 });
+      await page.waitForSelector(sizesSelector, { timeout: 0 });
+      const sizeDivs = Array.from(document.querySelectorAll(selector));
+      console.log(sizeDivs);
+
       await page.waitForFunction(({ selector, sizeStr }) => {
         const sizeDivs = Array.from(document.querySelectorAll(selector));
         console.log(sizeDivs);
